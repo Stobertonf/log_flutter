57e36a2bd1875d517555b0804566a645a7879734
stobertonf <stobertonf@gmail.com>
2025-04-13 14:26:39 -0300

    feat(Apis): Creating The Poke Api Service File.

---
+++ lib/services/poke_api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:log_flutter/models/pokemon_model.dart';
+
+class PokeApiService {
+  static Future<PokemonModel?> buscarPokemon(String nome) async {
+    try {
+      final response =
+          await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/$nome'));
+
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+        return PokemonModel.fromJson(json);
+      }
+    } catch (e) {
+      rethrow;
+    }
+    return null;
+  }
+}
